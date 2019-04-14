9887bf0813573226d99f6f69bbcf1936a6872306
Rick <rdsavage10@gmail.com>
2019-04-13 20:07:18 -0400

    ready for the next part

--- db/migrate/20190413212043_create_rooms.rb
+++ db/migrate/20190413212043_create_rooms.rb
@@ -3,8 +3,7 @@ class CreateRooms < ActiveRecord::Migration[5.2]
     create_table :rooms do |t|
       t.string :name
       t.text :description
-      t.text :link1
-      t.text :link2
+      t.text :path
 
       t.timestamps
     end
