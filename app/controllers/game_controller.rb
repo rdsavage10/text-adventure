1e9790a99d5274cbe927d26ea9eb7ef8ccdefd23
Rick <rdsavage10@gmail.com>
2019-04-14 14:35:25 -0400

    Add randomization

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,6 +3,19 @@ class GameController < ApplicationController
   def current_room
     @room = Room.find(params[:id])
     @paths = @room.path
+    @chance = []
+
+    @paths.each_with_index do |path, index|
+      if path.length > 2
+        if path[2] >= rand(100)
+          @chance[index] = true
+        else
+          @chance[index] = false
+        end
+      else
+        @chance[index] = "fuck"
+      end
+    end
   end
 
   def index
